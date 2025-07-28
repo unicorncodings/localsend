32566707fe4cbec416cdff7c1ee66dc5cb5a96e1
unicorncodings <unicorncomingsoon@gmail.com>
2025-07-28 13:29:20 +0530

    Create UploadIcon.tsx

---
+++ components/icons/UploadIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UploadIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path d="M5 20h14v-2H5v2zm7-18l-5 5h3v6h4v-6h3l-5-5z" />
+  </svg>
+);
+
+export default UploadIcon;
